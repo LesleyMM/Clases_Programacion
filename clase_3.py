a5ae05cc32ce9cfc192c0314a05a4da406301eb5
Lesley Mendoza Montes <leeslii138@gmail.com>
2023-05-03 18:00:20 -0700

    Clase_3

---
+++ clase_3.py
@@ -0,0 +1,28 @@
+
+if __name__ == '__main__': 
+    print('Hola, Mundo!')
+    print('Hola, yo soy Lesley y estoy aprendiendo Python')
+    print('Nombre\tEdad\tFecha de nacimiento')
+    print('Lesley\t24\t22/09/98')
+    print('==========================================================')
+    nombre = 'Kevin'
+    edad = "24"
+    fecha_nacimiento = '05/12/1998'
+    print('Nombre\tEdad\tFecha de nacimiento')
+    print('Lesley\t24\t22/09/1998')
+    print(nombre + '\t' + edad + '\t' + fecha_nacimiento)
+    print('===========================================================')
+    print('animal\tnumero de patas')
+    animal_1 = 'gato'
+    numero_de_patas_animal_1 = '4'
+    animal_2 = 'chango'
+    numero_de_patas_animal_2 = '2'
+    print(animal_1+ '\t' + numero_de_patas_animal_1)
+    print(animal_2+ '\t' + numero_de_patas_animal_2)
+    print('===========================================================')
+    Nombre=input("Introduce tu nombre:")
+    Edad=input("Introduce tu edad")
+    Fecha_de_nacimiento=input("Introduce tu fecha de nacimiento")
+    print('Nombre\tEdad\tFecha de nacimiento')
+    print(Nombre+'\t' + Edad+ '\t' + Fecha_de_nacimiento)
+    #Lo que quieras
