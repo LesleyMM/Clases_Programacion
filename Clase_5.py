64188294b1961a032cccfae7d098481cbb6773e8
Lesley Mendoza Montes <leeslii138@gmail.com>
2023-05-10 17:39:13 -0700

    Clase_5

---
+++ Clase_5.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    mi_variable = 20
+    numero_1 = int(input('Cual es el numero 1:'))
+    numero_2 = int(input('Cual es el numero 2:'))
+    if numero_1 == numero_2:
+        print('el numero_1 y el numero_2 son iguales')
+
+    if numero_1 > numero_2:
+        print('el numero_1 es mayor al numero_2')
+
+    if numero_1 < numero_2:
+        print('el numero_1 es menor al numero_2')
+
+      
